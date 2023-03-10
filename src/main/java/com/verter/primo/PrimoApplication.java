bc5af9fbf6745aeb509f275eef04bb3fcac23bb1
VerterDesp <allaknar@gmail.com>
2023-03-11 00:08:35 +0200

    first commit;

---
+++ src/main/java/com/verter/primo/PrimoApplication.java
@@ -0,0 +1,13 @@
+package com.verter.primo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Primo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Primo.class, args);
+	}
+
+}
