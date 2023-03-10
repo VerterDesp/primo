8f5550fb31b00fcaa366e69ee80b988999bc45ef
VerterDesp <allaknar@gmail.com>
2023-03-11 00:46:32 +0200

    five commit;

---
+++ src/test/java/com/verter/primo/PrimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verter.primo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
