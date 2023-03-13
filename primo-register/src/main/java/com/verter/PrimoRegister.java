16f87602922403a21a69c85994e47c09d6fe0d32
VerterDesp <allaknar@gmail.com>
2023-03-14 01:13:45 +0200

    register service - init commit;

---
+++ primo-register/src/main/java/com/verter/PrimoRegister.java
@@ -0,0 +1,11 @@
+package com.verter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoRegister {
+  public static void main(String[] args) {
+    SpringApplication.run(PrimoRegister.class, args);
+  }
+}
