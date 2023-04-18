0cdd80499bb9a362fa615a3e716eb4164a4051d0
VerterDesp <allaknar@gmail.com>
2023-04-18 21:54:11 +0300

    email sender micro-service init

---
+++ primo-email-sender/src/main/java/com/verter/PrimoEmailSender.java
@@ -0,0 +1,11 @@
+package com.verter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoEmailSender {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimoEmailSender.class, args);
+    }
+}
