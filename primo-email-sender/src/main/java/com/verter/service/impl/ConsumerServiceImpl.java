c7a3c6c2782706b004c65d80b8baa0f1df5dac30
VerterDesp <allaknar@gmail.com>
2023-04-24 18:10:25 +0300

    move configs to config server; add email sender to service registry;

--- primo-email-sender/src/main/java/com/verter/service/impl/ConsumerServiceImpl.java
+++ primo-email-sender/src/main/java/com/verter/service/impl/ConsumerServiceImpl.java
@@ -2,15 +2,18 @@ package com.verter.service.impl;
 
 import com.verter.dto.PurchaseDTO;
 import com.verter.service.ConsumerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ConsumerServiceImpl implements ConsumerService {
 
     @RabbitListener(queues = "${spring.rabbitmq.queue}")
     @Override
     public void receivedMessage(PurchaseDTO dto) {
+        log.info("new message in email sender");
         System.out.println(dto);
     }
 }
