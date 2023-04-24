785fd52a550685917932f28513445aeb22b92d03
VerterDesp <allaknar@gmail.com>
2023-04-24 18:08:50 +0300

    move configs to config server; add payment to service registry;

--- primo-payment/src/main/java/com/verter/controller/ProducerController.java
+++ primo-payment/src/main/java/com/verter/controller/ProducerController.java
@@ -3,12 +3,14 @@ package com.verter.controller;
 import com.verter.dto.PurchaseDTO;
 import com.verter.service.ProducerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping("/payment")
 public class ProducerController {
@@ -17,6 +19,7 @@ public class ProducerController {
 
     @PostMapping("/notify")
     public void sendMessage(@RequestBody PurchaseDTO dto) {
+        log.info("new message FROM payment to email-sender");
         producerService.sendMessage(dto);
     }
 }
